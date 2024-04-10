7db61f8cb56751fe81d1920161dc4a3a7b8cab6b
0x1Shub <shubhamyeram13@gmail.com>
2024-04-10 18:58:45 +0530

    Created: Folder Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import Loader from './components/Loader';
+
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const  Products = lazy(() => import('./pages/Products'));
+const  Transactions = lazy(() => import('./pages/Transactions'));
+const  Customer = lazy(() => import('./pages/Customer'));
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path='/admin/dashboard' element={<Dashboard />}/>
+        <Route path='/admin/product' element={<Products />}/>
+        <Route path='/admin/transaction' element={<Transactions />}/>
+        <Route path='/admin/customer' element={<Customer />}/>
+      </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
