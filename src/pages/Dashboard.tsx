fa2c1a0cf4a7c5f3894c5abb908cb1c9f6afbdf2
0x1Shub <shubhamyeram13@gmail.com>
2024-04-12 19:00:57 +0530

    Created: Doughnut Chart

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,7 +4,8 @@ import { BsSearch } from 'react-icons/bs'
 import userImg from '../assets/userpic.png';
 import { HiTrendingUp, HiTrendingDown } from "react-icons/hi";
 import data from '../assets/data.json';
-import { BarChart } from "../components/Charts";
+import { BarChart, DoughnutChart } from "../components/Charts";
+import { BiMaleFemale } from "react-icons/bi";
  
 const Dashboard = () => {
   return (
@@ -47,6 +48,17 @@ const Dashboard = () => {
             </div>
           </div>
         </section>
+
+        <section className="transaction-container">
+            <div className="gender-chart">
+              <h2>Gender Ratio</h2>
+              <DoughnutChart labels={['Female', "Male"]} data={[12, 19]} backgroundColor={["hsl(340,82%,56%)", "rgba(53,162,235,0.8)"]}
+              cutout={90} />
+              <p><BiMaleFemale /></p>
+            </div>
+
+            {/* Table */}
+        </section>  
 
       </main>
     </div>
