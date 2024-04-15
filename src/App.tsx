59e086c0d6a12fec6ffa511a7bb224d0452c2297
0x1Shub <shubhamyeram13@gmail.com>
2024-04-15 20:14:14 +0530

    Created: Bar Chart

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Loader from './components/Loader';
 import NewProduct from './pages/management/NewProduct';
 import ProductManagement from './pages/management/ProductManagement';
 import TransactionManagement from './pages/management/TransactionManagement';
+import BarCharts from './pages/charts/BarCharts';
+import PieCharts from './pages/charts/PieCharts';
+import LineCharts from './pages/charts/LineCharts';
 
 const Dashboard = lazy(() => import('./pages/Dashboard'));
 const  Products = lazy(() => import('./pages/Products'));
@@ -21,9 +24,14 @@ const App = () => {
         <Route path='/admin/transaction' element={<Transactions />}/>
         <Route path='/admin/customer' element={<Customer />}/>
 
+        <Route path="/admin/chart/bar" element={<BarCharts />} />
+        <Route path="/admin/chart/pie" element={<PieCharts />} />
+        <Route path="/admin/chart/line" element={<LineCharts />} />
+
+
         <Route path="/admin/product/new" element={<NewProduct />} />
-          <Route path="/admin/product/:id" element={<ProductManagement />} />
-          <Route path="/admin/transaction/:id" element={<TransactionManagement />}/>
+        <Route path="/admin/product/:id" element={<ProductManagement />} />
+        <Route path="/admin/transaction/:id" element={<TransactionManagement />}/>
       </Routes>
       </Suspense>
     </BrowserRouter>
